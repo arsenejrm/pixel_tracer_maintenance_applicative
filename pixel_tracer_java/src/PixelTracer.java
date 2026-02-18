4bae26634b023836be7d1146c5543da5f7e97f01
arsenejrm <ajerome.git@gmail.com>
2026-02-18 18:21:51 +0100

    Fix no command error + minor fixes

--- pixel_tracer_java/src/PixelTracer.java
+++ pixel_tracer_java/src/PixelTracer.java
@@ -99,6 +99,7 @@ public class PixelTracer {
 
     /**
      */
+    @Override
     public String toString()
     {
         return "PixelTracer{" +
@@ -127,64 +128,76 @@ public class PixelTracer {
 
 
     /**
-     * @return       PixelTracer
-     */
-    public PixelTracer PixelTracer()
-    {
-        return new PixelTracer();
-    }
-
-
-    /**
      */
     public void clear_screen()
     {
     }
 
     public String command_interpreter(String user_input) {
+        String error_empty = "commandde manquante";
+        String error_param = "erreur paramètres, consulter la commande help";
+        
         String[] splitted_input = user_input.split(" ");
+        if (splitted_input.length == 0) {
+            return error_empty;
+        }
         String command_name = splitted_input[0];
         ArrayList<String> command_params = new ArrayList<>();
         for (int i = 1; i < splitted_input.length; i++) {
             command_params.add(splitted_input[i]);
         }
-        String error_param = "erreur paramètres, consulter la commande help";
 
-        if (command_name.equals("help")) {
-            if (!command_params.isEmpty()) {
-                return error_param;
+        switch (command_name) {
+            case "" -> {
+                return error_empty;
             }
-            return  "        **************************************************\r\n" + //
-                    "        ****         VECTOR TEXT-BASED EDITOR         ****\r\n" + //
-                    "        **************************************************\r\n" + //
-                    "        ==== Control ====\r\n" + //
-                    "        plot : draw dcreen\r\n" + //
-                    "        clear : clear screen\r\n" + //
-                    "        exit : quitter le programme\r\n" + //
-                    "        ==== Draw shapes ====\r\n" + //
-                    "        point px py : create point a position (px, px)\r\n" + //
-                    "        line x1 y1 x2 x2 : draw line from (x1, y1) to (x1, y1)\r\n" + //
-                    "        square x1 y1 l : draw square (x1, y1)  length\r\n" + //
-                    "        rectangle x1 y1 w h : draw square (x1, y1)  width height\r\n" + //
-                    "        circle x y r : center at (x, y) radus r\r\n" + //
-                    "        polygon x1 y1 x2 y2 ... : draw polygon\r\n" + //
-                    "        curve x1 y1 x2 y2 x3 y3 x4 y4 : draw Bezier curve\r\n" + //
-                    "        ==== Draw manager ====\r\n" + //
-                    "        list {layers, arias, shapes}\r\n" + //
-                    "        select {aria, layer} {id}\r\n" + //
-                    "        delete {aria, layer, shape} {id}\r\n" + //
-                    "        new {aria, layer}\r\n" + //
-                    "        ==== Set ====\r\n" + //
-                    "        set char {border, background} ascii_code\r\n" + //
-                    "        set layer {visible, unvisible} {id}";
-        } else if (command_name.equals("plot")) {
-            return "Bam t'as l'affichage (c'est faux)";
-        } else if (command_name.equals("clear")) {
-            return "Bam tout l'affichage dégage (c'est faux)";
-        } else if (command_name.equals("exit")) {
-            return "Bam fin de programme (c'est faux)";
+            case "help" -> {
+                if (!command_params.isEmpty()) {
+                    return error_param;
+                }
+                return  """
+                    \t**************************************************\r
+                    \t****         VECTOR TEXT-BASED EDITOR         ****\r
+                    \t**************************************************\r
+                    \t==== Control ====\r
+                    \tplot : draw dcreen\r
+                    \tclear : clear screen\r
+                    \texit : quitter le programme\r
+                    \t==== Draw shapes ====\r
+                    \tpoint px py : create point a position (px, px)\r
+                    \tline x1 y1 x2 x2 : draw line from (x1, y1) to (x1, y1)\r
+                    \tsquare x1 y1 l : draw square (x1, y1)  length\r
+                    \trectangle x1 y1 w h : draw square (x1, y1)  width height\r
+                    \tcircle x y r : center at (x, y) radus r\r
+                    \tpolygon x1 y1 x2 y2 ... : draw polygon\r
+                    \tcurve x1 y1 x2 y2 x3 y3 x4 y4 : draw Bezier curve\r
+                    \t==== Draw manager ====\r
+                    \tlist {layers, arias, shapes}\r
+                    \tselect {aria, layer} {id}\r
+                    \tdelete {aria, layer, shape} {id}\r
+                    \tnew {aria, layer}\r
+                    \t==== Set ====\r
+                    \tset char {border, background} ascii_code\r
+                    \tset layer {visible, unvisible} {id}
+                """;
+            }
+                        
+            case "plot" -> {
+                return "[WIP] Bam t'as l'affichage (c'est faux)";
+            }
+
+            case "clear" -> {
+                return "[WIP] Bam tout l'affichage dégage (c'est faux)";
+            }
+
+            case "exit" -> {
+                return "[WIP] Bam fin de programme (c'est faux)";
+            }
+
+            default -> {
+                return "command = " + command_name +
+                        "Commande inconnue. Tapez 'help' pour voir les commandes disponibles.";
+            }
         }
-        return "command = " + command_name + 
-            "Commande inconnue. Tapez 'help' pour voir les commandes disponibles.";
     }
 }
